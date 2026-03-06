4ebbc9b43757075c8def36fbfbca151ca6f7b133
AkkaRin11 <artemcrepper@gmail.com>
2026-03-06 16:37:41 +0300

    init

---
+++ src/main/java/ru/gigasigma/blpscrud/BlpsCrudApplication.java
@@ -0,0 +1,13 @@
+package ru.gigasigma.blpscrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlpsCrudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlpsCrudApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/gigasigma/blpscrud/BlpsCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.gigasigma.blpscrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlpsCrudApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
